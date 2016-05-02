14e24f36f2a9a6fa6a52bd67452e20eeb79ac84b
Torben <Torben@jastrow.info>
2016-05-02 14:33:50 +0200

    Added very simple JWT auth
    Restricted & non-restricted routes: /api/restricted & /api
    login: /login POST username: duck & password: duck

--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
@@ -19,6 +21,32 @@ func helloHandler(c echo.Context) error {
 }
 func messageHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, response{Message: c.Param("message")})
+}
+
+func loginHandler(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	if username == "duck" && password == "duck" {
+		// Create token
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		// Set claims
+		token.Claims["name"] = "Jon Snow"
+		token.Claims["admin"] = true
+		token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, map[string]string{
+			"token": t,
+		})
+	}
+
+	return echo.ErrUnauthorized
 }
 
 func main() {
@@ -38,9 +66,21 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	//username: duck  password:duck
+	e.POST("/login", loginHandler)
+	//create sub-router for api functions
+	api := e.Group("/api")
+
 	//set routes for api
-	e.GET("/api", helloHandler)
-	e.GET("/api/:message", messageHandler)
+	api.GET("", helloHandler)
+
+	api.GET("/:message", messageHandler)
+
+	//create restricted sub-router
+	restricted := api.Group("/restricted", middleware.JWTAuth([]byte("secret")))
+	//set restricted routes for api
+	restricted.GET("", helloHandler)
+	restricted.GET("/:message", messageHandler)
 
 	// serves the static files
 	e.Static("/", webDir)
