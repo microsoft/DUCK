92cc72cde56eb37e2f2c50623839c1a6103e3ba1
jmarino <jim.marino@gmail.com>
2016-05-11 21:38:36 +0200

    add data use service methods

--- frontend/src/assets/js/gateway/gateway.document.js
+++ frontend/src/assets/js/gateway/gateway.document.js
@@ -7,6 +7,11 @@ gatewayModule.service('DataUseDocumentService', function (CurrentUser, $http, $q
 
     var context = this;
 
+    /**
+     * Retrieves summaries for data use statement documents authored by the current user.
+     *
+     * @return an array of summaries
+     */
     this.getAuthoredDocumentSummaries = function () {
         return $q(function (resolve, reject) {
             // make sure the user is signed in is set
@@ -32,7 +37,33 @@ gatewayModule.service('DataUseDocumentService', function (CurrentUser, $http, $q
 
     };
 
+    /**
+     * Retrieves a data use statement document authored by the current user.
+     *
+     * @param documentId the document id
+     * @return the document
+     */
     this.getDocument = function (documentId) {
+        return $q(function (resolve, reject) {
+            // make sure the user is signed in is set
+            if (!CurrentUser.loggedIn) {
+                $state.go('signin');
+            }
+            var url = "/v1/documents/" + CurrentUser.id + "/" + documentId;
 
-    }
+            // disable server call until implemented
+            if (true) {
+                resolve({name: "Document 1", statements: [{"order": 1, "Statement 1"}, {"order": 2, "Statement 2"}]});
+                return;
+            }
+
+            //noinspection JSUnusedLocalSymbols
+            $http.get(url).success(function (data, status, headers, config) {
+                var document = angular.fromJson(data);
+                resolve(document);
+            }).error(function (data, status, headers, config) {
+                reject(status);
+            });
+        });
+    };
 });
