6c765333d080291d23f4d9bd2b0a16e69ac8c58b
jmarino <jim.marino@gmail.com>
2016-05-18 16:16:05 +0200

    add autocomplete component and fuzzy search

--- frontend/src/assets/modules.js
+++ frontend/src/assets/modules.js
@@ -6,7 +6,7 @@ angular.module("duck.component", []);
 angular.module("duck.i18n", []);
 angular.module("duck.gateway", []);
 angular.module("duck.main", ["ui.router", "ngAnimate"]);
-angular.module("duck.editor", ["ui.router"]);
+angular.module("duck.editor", ["MassAutoComplete", "ui.router"]);
 angular.module("duck.signin", ["ui.router"]);
 angular.module("duck.home", ["ui.router"]);
 
