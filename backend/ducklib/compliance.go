1d334b462813ade715e39cb9b94b1918a1fc60c4
Tom Gordon <thomas.gordon@fokus.fraunhofer.de>
2016-06-07 13:43:36 +0200

    revised the compliance API to reference some structures using pointers and add a reference to the couchDB database to the parameters of the GetTheory method

--- backend/ducklib/compliance.go
+++ backend/ducklib/compliance.go
@@ -18,26 +18,26 @@ const (
 // the Carneades Argumentation System
 
 type ComplianceChecker interface {
-	/* Check does the following:
-		   * Reads the data use document from its given io.Reader
-		   * Translates the data use statements in the document into Carneades assumptions (terms)
-	       * Applies the theory to the assumptions, using the Carneades inference engine,
-	         to construct a Carneades argument graph
-		   * Evaluates the argument graph to label the statements in the graph in, out or undecided.
-		   * Returns the evaluated argument graph
-		  If there are not errors, nil is returned.  Otherwise an error is returned
-		  describing the error.
-	*/
-	Check(ruleBase caes.Theory, dataUseDocument io.Reader) (caes.ArgGraph, error)
-
 	// GetTheory: Retrieve the theory for the given ruleBaseId. If the
 	// revision is not equal to the revision used to compile the theory,
 	// the theory is first updated, by downloading the given revision from
 	// the document database and compiling the rulebase into a theory.
 	// If there are no errors, the returned error will be nil.
-	GetTheory(ruleBaseId string, revision string) (caes.Theory, error)
+	GetTheory(db Database, ruleBaseId string, revision string) (caes.Theory, error)
 
-	isCompliant(ag caes.ArgGraph) bool
-	nonCompliantDataUseStatements(ag caes.ArgGraph) []Statement
-	Render(ag caes.ArgGraph, format ArgMapFormat, w io.Writer) error
+	/* Check does the following:
+		* Reads the data use document from its given io.Reader
+		* Translates the data use statements in the document into Carneades assumptions (terms)
+	    * Applies the theory to the assumptions, using the Carneades inference engine,
+	      to construct a Carneades argument graph
+		* Evaluates the argument graph to label the statements in the graph in, out or undecided.
+		* Returns the evaluated argument graph
+		If there are not errors, nil is returned.  Otherwise an error is returned
+		describing the error.
+	*/
+	Check(ruleBase *caes.Theory, document *Document) (*caes.ArgGraph, error)
+
+	isCompliant(ag *caes.ArgGraph) bool
+	nonCompliantDataUseStatements(ag *caes.ArgGraph) []Statement
+	Render(ag *caes.ArgGraph, format ArgMapFormat, w io.Writer) error
 }
